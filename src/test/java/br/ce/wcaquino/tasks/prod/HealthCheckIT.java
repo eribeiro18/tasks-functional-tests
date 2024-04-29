3ea9aaa316fc97f8c37121ff81c9b0faac3e8224
Evandro Ribeiro <eribeiro18@gmail.com>
2024-04-29 09:39:14 -0300

    ajustes

--- src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/ce/wcaquino/tasks/prod/HealthCheckIT.java
@@ -18,7 +18,7 @@ public class HealthCheckIT {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.176:4444/wd/hub"), cap);
 		try {
-			driver.navigate().to("http://192.168.0.15:9999/tasks");
+			driver.navigate().to("http:/192.168.1.176:9999/tasks");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			String version = driver.findElement(By.id("version")).getText();
 			Assert.assertTrue(version.startsWith("build"));
